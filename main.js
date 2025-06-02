72a504468a26a3265f92af6002d5fd89ea19636f
Cassie Hopkin <cassie.hopkin13@gmail.com>
2025-06-02 09:16:02 -0700

    added blurbs

--- main.js
+++ main.js
@@ -274,7 +274,7 @@ Promise.all(csvs.map(file => d3.csv(file)))
 
         //Create svg
         const svg = d3.select(`#svg${i}`)
-          .append("svg")
+          .insert("svg", ".my-box")
           .attr("viewBox", [0, 0, width, height]);
           
         //X scale (linear)
